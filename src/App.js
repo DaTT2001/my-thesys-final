6d5b5aceefd30e3d234eb3ba3a50929b361760ab
Duong Cong Dat <104434271+DaTT2001@users.noreply.github.com>
2023-11-18 08:42:27 +0700

    update notification

--- src/App.js
+++ src/App.js
@@ -48,6 +48,9 @@ function App() {
       try {
         const response = await axios.get(`https://blynk.cloud/external/api/get?token=${API_TOKEN}&v3`);
         setGas(response.data)
+        if(response.data > 100) {
+          handleNotificationClick()
+        }
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -59,6 +62,24 @@ function App() {
     // Hủy interval khi component bị unmount
     return () => clearInterval(intervalId);
   }, []);
+
+  const handleNotificationClick = () => {
+    if ('Notification' in window) {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          const notification = new Notification('Nguy hiểm', {
+            body: 'Lượng khí gas vượt quá mức cho phép! hãy ra khỏi phòng ngay lập tức',
+          });
+          const audio = new Audio('/sss.mp3');
+          audio.play();
+          if ('vibrate' in navigator) {
+            navigator.vibrate([200, 100, 200]);
+          }
+        }
+      });
+    }
+  };
+
   return (
     <div className="App">
         <h2 className="main-header">Environmental parameters</h2>
